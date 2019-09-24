268d10036948bb5cebc2b5285d42a1e6e6fa281b
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-24 02:13:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,11 @@
-
-  class Song 
-    attr_accessor :title
+class Artist 
+  attr_accessor :name
+end
+class Song 
+  attr_accessor :title
     
-
-  
+  def initialize(name)
+    @name = name
   end
+
+end
